82c004442d267307d6962877547a8bf3c10fd9aa
Chillara, Rishi Maruti <rishichillara@gatech.edu>
2021-04-19 23:18:03 -0400

    Test Version of Video Stream connecting to Motor Controls

--- controller.py
+++ controller.py
@@ -1,5 +1,6 @@
-from detect import VideoStream
+from detect import VideoStreamWrapper
 from fire import aim
+import argparse
 
 import RPi.GPIO as GPIO
 
@@ -8,12 +9,32 @@ GPIO.setwarnings(False)
 
 GPIO.setup(OFF_PIN, GPIO.IN)
 
+# Define and parse input arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--modeldir', help='Folder the .tflite file is located in',
+                    required=True)
+parser.add_argument('--graph', help='Name of the .tflite file, if different than detect.tflite',
+                    default='detect.tflite')
+parser.add_argument('--labels', help='Name of the labelmap file, if different than labelmap.txt',
+                    default='labelmap.txt')
+parser.add_argument('--threshold', help='Minimum confidence threshold for displaying detected objects',
+                    default=0.5)
+parser.add_argument('--resolution', help='Desired webcam resolution in WxH. If the webcam does not support the resolution entered, errors may occur.',
+                    default='1280x720')
+parser.add_argument('--edgetpu', help='Use Coral Edge TPU Accelerator to speed up detection',
+                    action='store_true')
+
+args = parser.parse_args()
+
+videoStream = VideoStreamWrapper()
+videoStream.startStream(args.modeldir, args.graph, args.label, args.threshold, args.resolution, args.edgetpu)
+
 #Switch to turn off program
 OFF_PIN = 15;
 OFF = (GPIO.input(15) + 1)%2
 
 while (GPIO.input(OFF_PIN) != OFF):
-    vs = VideoStream()
-    aim(vs.detect)
-    
+    if (videoStream.detect != ""):
+        aim(videoStream.detect)
+        videoStream.resetDetect
     
