a1a95c6eceaf825c74e66095069353ee2b057305
Chillara, Rishi Maruti <rishichillara@gatech.edu>
2021-04-20 21:57:45 -0400

    hotfix

--- controller.py
+++ controller.py
@@ -37,8 +37,10 @@ OFF = (GPIO.input(OFF_PIN) + 1)%2
 while (True):
     # print("entered loop")
     # print("detect is blank") if videoStream.detect == "" else print("detect is not blank")
+    time.sleep(1)
     if (videoStream.detect != ""):
         print("VS Detect: ",videoStream.detect)
         fire.aim(videoStream.detect)
         videoStream.resetDetect
+
     
